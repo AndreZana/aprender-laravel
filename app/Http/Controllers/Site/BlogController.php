55c7ac269dc50b450e82ba96f329dcb55d71dc61
André <andre.zana@h2opurificadores.com>
2024-05-20 16:03:57 -0300

    adicionando imagens, css, html, tudo de forma estatica

---
+++ app/Http/Controllers/Site/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('site.blog.index');
+    }
+    public function index()
+    {
+        return view('site.blog.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +20,19 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::namespace(value:'Site')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('site.home');
+
+    Route::get('produtos', [CategoryController::class, 'index'])->name('site.products');
+    Route::get('produtos/{category}', [CategoryController::class, 'show'])->name('site.products.category');
+
+    Route::get('blog', [BlogController::class, 'index'])->name('site.blog');
+
+    Route::view('sobre', 'site.about.index')->name('site.about');
+
+    Route::get('contato', [ContactController::class,'index'])->name('site.contact');
+    Route::post('contato', [ContactController::class,'form'])->name('site.contact.form');
+
+});
